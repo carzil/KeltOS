9ce118d68ee72549ea2b6663cb595a48beb557a2
Alexander Andreev <carzil@yandex.ru>
2017-02-13 13:22:58 +0300

    control passed immediately to kmain, kernel stack located at the end of available SRAM

--- include/kernel/defs.h
+++ include/kernel/defs.h
@@ -6,7 +6,9 @@
  */
 
 #define STACK_SIZE      0x2000
-
 #define SRAM_BASE       0x20000000
+#define SRAM_SIZE       0x20000
+#define STACK_END       (SRAM_BASE + SRAM_SIZE)
+#define STACK_START     (SRAM_BASE + SRAM_SIZE - STACK_START)
 
 #endif
