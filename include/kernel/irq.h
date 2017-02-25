8f152c939e742cc0f508eb97a190c35a99729720
Alexander Andreev <carzil@yandex.ru>
2017-02-25 21:55:35 +0300

    added ISR macros

--- include/kernel/irq.h
+++ include/kernel/irq.h
@@ -3,7 +3,12 @@
 
 #include "kernel/types.h"
 
+/* interrupt service routine mark */
+#define ISR_FUNCTION __attribute__((naked))
+
 extern void _handler_trampoline();
-u32 swi_handler();
+
+#define EXCEPTION_RETURN(code) asm("mov lr, %0\nbx lr\n" : : "r"(code) : );
+#define EXCEPTION_RETURN_DEFAULT() asm("bx lr");
 
 #endif
