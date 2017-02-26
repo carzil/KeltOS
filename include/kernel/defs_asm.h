8b89a67781dd31d67954d2d1c234c528bcbae3e8
Alexander Andreev <carzil@yandex.ru>
2017-02-27 01:40:18 +0300

    added macros for asm

---
+++ include/kernel/defs_asm.h
@@ -0,0 +1,14 @@
+#ifndef _KELT_DEFS_ASM_H_
+#define _KELT_DEFS_ASM_H_
+
+
+#include "kernel/defs.h"
+
+#define FUNCTION(name) \
+    .globl name; \
+    .type name, %function; \
+name
+
+.syntax unified
+
+#endif
