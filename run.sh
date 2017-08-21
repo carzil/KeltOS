3711f94e806c96444bbc90fc41dc047c7fb30db9
Alexander Andreev <carzil@yandex.ru>
2017-08-22 00:36:56 +0200

    run.sh update

--- run.sh
+++ run.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
-/opt/gnuarmeclipse/qemu/bin/qemu-system-gnuarmeclipse -M STM32-E407 -cpu cortex-m4 -kernel \
-kernel.elf -s --verbose --verbose --semihosting-config enable=on,target=native -d \
-unimp,guest_errors
+/opt/gnuarmeclipse/qemu/bin/qemu-system-gnuarmeclipse \
+    -M STM32F4-Discovery \
+    -cpu cortex-m4 \
+    -kernel kelt.bin \
+    -serial stdio \
+    --verbose --verbose \
+    --semihosting-config enable=on,target=native -d unimp,guest_errors \
+    -s \
+    $@
