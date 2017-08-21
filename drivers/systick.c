368ffb95eda7617b0e75052190a213206375ca33
Alexander Andreev <carzil@yandex.ru>
2017-08-22 00:35:49 +0200

    SysTick reload value must not be 0 on enabling

--- drivers/systick.c
+++ drivers/systick.c
@@ -1,4 +1,5 @@
 #include "kernel/types.h"
+#include "kernel/panic.h"
 #include "kernel/timer.h"
 #include "drivers/systick.h"
 #include "drivers/semihosting.h"
@@ -19,11 +20,16 @@ void NAKED systick_handler()
 void systick_init()
 {
     c_tick = 0;
-    /* Enable SysTick and its interrupts */
-    *(volatile u32*)SYSTICK_CSR = SYSTICK_ENABLE | SYSTICK_INT;
     /*
      * By default SysTick operates on the same frequency as the processor.
      * Don't forget to subtract 1, cause in [0, n-1] there are n numbers.
      */
     *(volatile u32*)SYSTICK_RVR = (CPU_CLOCK_RATE_HZ / TICK_RATE_HZ) - 1;
+
+    /* Enable SysTick and its interrupts */
+    *(volatile u32*)SYSTICK_CSR = SYSTICK_ENABLE | SYSTICK_INT;
+
+    if (!(*(volatile u32*)SYSTICK_CSR & SYSTICK_ENABLE)) {
+        panic("cannot enable SysTick");
+    }
 }
