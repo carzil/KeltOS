39093eba084de8d05a7a064bc76c22f43e82c979
pixelindigo <m4s4n0bu@gmail.com>
2017-08-26 21:42:26 +0300

    implemented basic BUG macro without stacktrace

--- include/kernel/bug.h
+++
@@ -1,9 +0,0 @@
-#ifndef _KELT_KERNEL_BUG_H_
-#define _KELT_KERNEL_BUG_H_
-
-#define BUG_ON(e)
-#define BUG_ON_NULL(e)
-#define BUG_ON_ZERO(e)
-#define BUG_ON_REACH()
-
-#endif
--- include/kernel/panic.h
+++ include/kernel/panic.h
@@ -2,4 +2,12 @@
 
 #include "kernel/defs.h"
 
+#define BUG() panic("bug");
+#define BUG_ON(e) if (e) { BUG() }
+/* TBD
+#define BUG_ON_NULL(e)
+#define BUG_ON_ZERO(e)
+#define BUG_ON_REACH()
+*/
+
 void panic(const char* msg);
