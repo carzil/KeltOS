843d90663128f2f1b88aa3a8af42184dffc11a98
pixelindigo <m4s4n0bu@gmail.com>
2017-08-26 22:41:41 +0300

    issue #14: fixed __LINE__ and __FILE__ usage

--- include/kernel/panic.h
+++ include/kernel/panic.h
@@ -2,7 +2,10 @@
 
 #include "kernel/defs.h"
 
-#define BUG_ON_REACH() printk("BUG occured at %s, line %d", __FILE__, __LINE__); panic("bug");
+#define STR(a) #a
+#define XSTR(a) STR(a)
+
+#define BUG_ON_REACH() panic("BUG occured at " __FILE__ ", line " XSTR(__LINE__) "\n");
 #define BUG_ON(e) if (e) { BUG_ON_REACH() }
 #define BUG_ON_NULL(e) if (e == NULL) { BUG_ON_REACH() }
 #define BUG_ON_ZERO(e) if (e == 0) { BUG_ON_REACH() }
