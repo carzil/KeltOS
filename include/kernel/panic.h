6d1aef0a7e8291334be1262a2383edffec174160
pixelindigo <m4s4n0bu@gmail.com>
2017-08-26 22:57:46 +0300

    issue #14: hardened BUG macros

--- include/kernel/panic.h
+++ include/kernel/panic.h
@@ -7,7 +7,7 @@
 
 #define BUG_ON_REACH() panic("BUG occured at " __FILE__ ", line " XSTR(__LINE__) "\n");
 #define BUG_ON(e) if (e) { BUG_ON_REACH() }
-#define BUG_ON_NULL(e) if (e == NULL) { BUG_ON_REACH() }
-#define BUG_ON_ZERO(e) if (e == 0) { BUG_ON_REACH() }
+#define BUG_ON_NULL(e) if ((e) == NULL) { BUG_ON_REACH() }
+#define BUG_ON_ZERO(e) if ((e) == 0) { BUG_ON_REACH() }
 
 void panic(const char* msg);
