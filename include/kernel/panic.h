1fa837797ff0835fd638311209198fb91417a31b
pixelindigo <m4s4n0bu@gmail.com>
2017-08-26 22:27:44 +0300

    issue #14: implemented BUG macros

--- include/kernel/panic.h
+++ include/kernel/panic.h
@@ -2,10 +2,9 @@
 
 #include "kernel/defs.h"
 
-#define BUG() panic("bug");
-#define BUG_ON(e) if (e) { BUG() }
-#define BUG_ON_NULL(e) if (e == NULL) { BUG() }
-#define BUG_ON_ZERO(e) if (e == 0) { BUG() }
-#define BUG_ON_REACH() BUG()
+#define BUG_ON_REACH() printk("BUG occured at %s, line %d", __FILE__, __LINE__); panic("bug");
+#define BUG_ON(e) if (e) { BUG_ON_REACH() }
+#define BUG_ON_NULL(e) if (e == NULL) { BUG_ON_REACH() }
+#define BUG_ON_ZERO(e) if (e == 0) { BUG_ON_REACH() }
 
 void panic(const char* msg);
